fe668127aa61236145a44e73bb345067ac7e2f4d
Cassandra5863 <93055245+Cassandra5863@users.noreply.github.com>
2024-09-04 20:31:56 -0400

    Create BankAccount.cs

---
+++ Week3/Exercise 1/BankAccount.cs
@@ -0,0 +1,7 @@
+public class BankAccount {
+  public string AccountNumber { get; set; }
+  public decimal CurBalance { get; set; }
+  public string AccountName { get; set; }
+  public string AccountHolder { get; set; }
+  public string AccountType { get; set; }
+}
